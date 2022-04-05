b2654b0623745f15e6c983341af4b3532f149424
h <1978732558@qq.com>
2022-04-05 05:20:07 -0700

    fix:Fixed an issue where suspended animation does not work

--- start.sh
+++ start.sh
@@ -6,14 +6,19 @@ qemu-system-x86_64 \
 -cpu qemu64 \
 -smp 1 \
 -kernel `pwd`/bzImage \
--append "root=/dev/sda console=ttyS0 rootfstype=ext4 rw" -m 200m \
+-append "root=/dev/sda rootfstype=ext4 rw kgdboc=ttyS1,115200" -m 200m \
 -drive file=`pwd`/rootfs.ext2 \
--nographic \
 -virtfs local,path=/home/h/linux/shared,mount_tag=host,security_model=none \
+-serial tcp::1234,server,nowait \
+#-nographic \
+#console=ttyS0 
+#-monitor telnet:127.0.0.1:5000,server,nowait \#open qemu monitor
+#-S -gdb tcp::1234
+#-append 'kgdbwait ' \
 #-fsdev local,id=test_dev,path=/data/host,security_model=none \
 #-device virtio-9p-ccw,fsdev=test_dev,mount_tag=test_mount \
 #-device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=Tsrc \
 #-fsdev local,security_model=passthrough,id=fsdev0,path=/data \
 #-net tap,ifname=tap1,script=/etc/qemu-ifup,downscript=no \
 #-device e1000,netdev=net0,mac=$macaddr -netdev tap,id=net0,script=/etc/qemu-ifup,\
-#-S -gdb tcp::9000 
+#console=ttyS1
